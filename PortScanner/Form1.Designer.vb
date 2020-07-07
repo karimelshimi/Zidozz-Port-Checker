1c5c9a6e4659471c92dde5e670290972f9d90191
Karim Elshimi <44316970+karimelshimi@users.noreply.github.com>
2020-07-07 05:25:50 +0200

    Add files via upload

---
+++ PortScanner/Form1.Designer.vb
@@ -0,0 +1,217 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.btnScan = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.nudStart = New System.Windows.Forms.NumericUpDown()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.nudStop = New System.Windows.Forms.NumericUpDown()
+        Me.lbPorts = New System.Windows.Forms.ListBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        CType(Me.nudStart, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.nudStop, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnScan
+        '
+        Me.btnScan.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnScan.Location = New System.Drawing.Point(206, 39)
+        Me.btnScan.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnScan.Name = "btnScan"
+        Me.btnScan.Size = New System.Drawing.Size(86, 57)
+        Me.btnScan.TabIndex = 0
+        Me.btnScan.Text = "Scan"
+        Me.btnScan.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(14, 11)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(92, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Target IP:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(108, 7)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(293, 24)
+        Me.TextBox1.TabIndex = 2
+        '
+        'nudStart
+        '
+        Me.nudStart.Location = New System.Drawing.Point(108, 39)
+        Me.nudStart.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.nudStart.Maximum = New Decimal(New Integer() {9999, 0, 0, 0})
+        Me.nudStart.Name = "nudStart"
+        Me.nudStart.Size = New System.Drawing.Size(91, 24)
+        Me.nudStart.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(14, 42)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(78, 20)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Start at:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(14, 75)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(75, 20)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Stop at:"
+        '
+        'nudStop
+        '
+        Me.nudStop.Location = New System.Drawing.Point(108, 71)
+        Me.nudStop.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.nudStop.Maximum = New Decimal(New Integer() {9999, 0, 0, 0})
+        Me.nudStop.Name = "nudStop"
+        Me.nudStop.Size = New System.Drawing.Size(91, 24)
+        Me.nudStop.TabIndex = 6
+        Me.nudStop.Value = New Decimal(New Integer() {9000, 0, 0, 0})
+        '
+        'lbPorts
+        '
+        Me.lbPorts.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbPorts.FormattingEnabled = True
+        Me.lbPorts.ItemHeight = 20
+        Me.lbPorts.Location = New System.Drawing.Point(17, 103)
+        Me.lbPorts.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.lbPorts.Name = "lbPorts"
+        Me.lbPorts.Size = New System.Drawing.Size(384, 264)
+        Me.lbPorts.TabIndex = 7
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(14, 401)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(163, 18)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "My Computer Name:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(173, 402)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(26, 18)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "00"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(14, 441)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(134, 18)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "My Computer IP:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(143, 442)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(26, 18)
+        Me.Label7.TabIndex = 12
+        Me.Label7.Text = "00"
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(321, 441)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(82, 42)
+        Me.Button2.TabIndex = 13
+        Me.Button2.Text = "About"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(416, 495)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.lbPorts)
+        Me.Controls.Add(Me.nudStop)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.nudStart)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnScan)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Zidozz Port Checker"
+        CType(Me.nudStart, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.nudStop, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnScan As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents nudStart As System.Windows.Forms.NumericUpDown
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents nudStop As System.Windows.Forms.NumericUpDown
+    Friend WithEvents lbPorts As System.Windows.Forms.ListBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+
+End Class
---
+++ PortScanner/Form1.vb
@@ -0,0 +1,45 @@
+﻿Imports System.Net.Sockets
+Imports System.Net
+Public Class Form1
+
+    Private Sub btnScan_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnScan.Click
+
+        Control.CheckForIllegalCrossThreadCalls = False 'Now you can Add items through a thread
+        For i As Integer = nudStart.Value To nudStop.Value
+            Dim tmpThread As New System.Threading.Thread(AddressOf ScanPort)
+            tmpThread.IsBackground = True
+            tmpThread.Start(i) 'i represents the current port 
+        Next
+    End Sub
+
+    Private Sub ScanPort(ByVal port As Integer)
+        Try 'Always put your code [or more complicated code] in a Try Catch Block :]
+            Dim tmpClient As New TcpClient()
+            Dim tmpEndPoint As New IPEndPoint(IPAddress.Parse(TextBox1.Text), port)
+            tmpClient.Connect(tmpEndPoint)
+            Threading.Thread.Sleep(1000) '50 is the Timeout in ms.
+            If tmpClient.Connected = True Then
+                lbPorts.Items.Add("Open Port: " & port) 'If Connected then it will be an open port
+            End If
+        Catch ex As Exception
+        End Try
+    End Sub
+
+    Private Sub lbPorts_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lbPorts.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim strIPAddress As String
+        Dim strHostName As String
+        strHostName = System.Net.Dns.GetHostName()
+        strIPAddress = System.Net.Dns.GetHostByName(strHostName).AddressList(0).ToString()
+        TextBox1.Text = strIPAddress
+        Label5.Text = strHostName
+        Label7.Text = strIPAddress
+
+
+    End Sub
+
+    
+End Class
